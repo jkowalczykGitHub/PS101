48e2f97408e8fa135cc061c59137e8d8887e891a
jkowalczykGitHub <jkowalczyk@ideaoutput.com>
2016-04-29 20:49:38 -0400

    Added files via upload

---
+++ Character_Array.ps1
@@ -0,0 +1,32 @@
+﻿clear-host
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted  -Verbose
+$aChar =  @()
+$aCharSet = @{}
+$aTerms = @()
+$aCatalog = @{}
+$strRootPath = "Z:\Blogs"
+$aCommonDelimeter = @(" ","_","-"," - ")
+
+$oFiles = Get-ChildItem $strRootPath
+$oFiles | ForEach-Object {
+    ForEach ($strDelimeter in $aCommonDelimeter) {
+        $aTerms += $_.BaseName.Split($strDelimeter)
+        if (-not $aCatalog.ContainsKey($strDelimeter)) {$aCatalog.Add($strDelimeter, $aTerms)}
+    }
+    $aChar = $_.BaseName.ToCharArray()
+    $aChar | ForEach-Object {
+        if (-not $aCharSet.ContainsKey($_)) { 
+            $aCharSet.Add($_, 1)
+        } else {
+            $aCharSet[$_] = $aCharSet[$_] + 1
+        }
+    }
+    $aTerms = $null
+}
+$aCharSet.GetEnumerator() | Sort-Object Value -Descending
+$aCatalog.GetEnumerator() | Sort-Object Key
+write-host "values"
+$aCatalog." - "
+$aChar = $null
+$aCharSet = $null
+$aCatalog = $null
---
+++ Iterative_Addition_to_100.ps1
@@ -0,0 +1,14 @@
+﻿$i = 0
+$x = 1
+
+
+clear host
+do {
+    $y = $i + $x
+    Write-Host $x " + " $i " = " $y
+    $i += $x
+    $x++
+}
+Until ($x -eq 101)
+#Write-Host $x " + " $i " = " $y
+Write-Host "x = " $x
